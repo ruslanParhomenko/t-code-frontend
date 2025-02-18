c700a987b5f549c7347f6134929d3d94fd7034cf
ruslan-parhomenko <parhomenkogm@gmail.com>
2025-02-18 12:16:13 +0200

    fix bg

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased  min-h-screen px-16 relative mx-auto `}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased  min-h-screen px-16 relative mx-auto bg-black text-white`}
       >
         <ClientLayout>{children}</ClientLayout>
       </body>
